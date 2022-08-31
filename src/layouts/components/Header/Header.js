881d3a5092a307865cc18a5c1d10c805827fbe0b
long <longtran24062000@gmail.com>
2022-08-31 15:46:56 +0700

    Responsive header 10%

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,11 +1,13 @@
 // Libraries
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
+import HeadlessTippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronRight, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronRight, faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
 // Path
 import config from '~/config';
+import Popper from '~/components/Popper';
 import Search from '~/layouts/components/Search';
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
@@ -28,9 +30,6 @@ function Header() {
       path: '/',
     },
   ];
-
-  
-
   return (
     <header className={cx('header')}>
       <nav className={cx('navbar')}>
@@ -50,8 +49,44 @@ function Header() {
             </div>
           </Menu>
         </div>
+        {/* Menu Icon */}
+        <button className={cx('navbar-toggler')}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
         {/* Actions */}
+        {/* <HeadlessTippy
+          render={(attrs) => (
+            <div className="box" tabIndex="-1" {...attrs}>
+              My tippy box
+            </div>
+          )}
+        >
+        <div>Long</div>
+        </HeadlessTippy> */}
         <div className={cx('actions')}>
+          {
+            <Popper>
+              <div className={cx('nav-wrapper')}>
+                <Link to={config.routes.home}>Trang chủ</Link>
+                <Link to={config.routes.forum}>Diễn đàn</Link>
+                <Link to={config.routes.teacherForm}>Cộng tác</Link>
+              </div>
+
+              <Search>
+                <button className={cx('search')}>
+                  <FontAwesomeIcon icon={faSearch} />
+                </button>
+              </Search>
+              <div className={cx('controller')}>
+                <Button className={cx('controller__button')} to={config.routes.login} outline>
+                  Đăng nhập
+                </Button>
+                <Button className={cx('controller__button')} to={config.routes.register} primary>
+                  Đăng ký
+                </Button>
+              </div>
+            </Popper>
+          }
           <div className={cx('nav-wrapper')}>
             <Link to={config.routes.home}>Trang chủ</Link>
             <Link to={config.routes.forum}>Diễn đàn</Link>
