f921d42b95e7cb22caa35a10c5e2018b5ece7cc9
=LongPro <longtran24062000@gmail.com>
2022-08-31 20:01:44 +0700

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './custom.scss';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from '~/App';
 import GlobalStyles from '~/components/GlobalStyles/GlobalStyles';
 import reportWebVitals from './reportWebVitals';
--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,13 +1,11 @@
 // Libraries
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
-import HeadlessTippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronRight, faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronRight, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 // Path
 import config from '~/config';
-import Popper from '~/components/Popper';
 import Search from '~/layouts/components/Search';
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
@@ -32,7 +30,7 @@ function Header() {
   ];
   return (
     <header className={cx('header')}>
-      <nav className={cx('navbar')}>
+      <nav className={cx('navbar','navbar-expand-lg navbar-light bg-light')}>
         {/* Logo + menu item */}
         <div className={cx('brand')}>
           {/* Logo */}
@@ -50,43 +48,61 @@ function Header() {
           </Menu>
         </div>
         {/* Menu Icon */}
-        <button className={cx('navbar-toggler')}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-        {/* Actions */}
-        {/* <HeadlessTippy
-          render={(attrs) => (
-            <div className="box" tabIndex="-1" {...attrs}>
-              My tippy box
-            </div>
-          )}
+        <button
+          className={cx('navbar-toggler')}
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-        <div>Long</div>
-        </HeadlessTippy> */}
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                to={config.routes.home}
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </Link>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link className="dropdown-item" to={config.routes.home}>
+                    Action
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to={config.routes.home}>
+                    Another action
+                  </Link>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <Link className="dropdown-item" to={config.routes.home}>
+                    Something else here
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+        {/* Actions */}
         <div className={cx('actions')}>
-          {
-            <Popper>
-              <div className={cx('nav-wrapper')}>
-                <Link to={config.routes.home}>Trang chủ</Link>
-                <Link to={config.routes.forum}>Diễn đàn</Link>
-                <Link to={config.routes.teacherForm}>Cộng tác</Link>
-              </div>
-
-              <Search>
-                <button className={cx('search')}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </button>
-              </Search>
-              <div className={cx('controller')}>
-                <Button className={cx('controller__button')} to={config.routes.login} outline>
-                  Đăng nhập
-                </Button>
-                <Button className={cx('controller__button')} to={config.routes.register} primary>
-                  Đăng ký
-                </Button>
-              </div>
-            </Popper>
-          }
           <div className={cx('nav-wrapper')}>
             <Link to={config.routes.home}>Trang chủ</Link>
             <Link to={config.routes.forum}>Diễn đàn</Link>
