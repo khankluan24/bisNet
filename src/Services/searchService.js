e774569bbc1c85d06c75c3da3ecfd8e9e119ce8e
long <longtran24062000@gmail.com>
2022-09-21 08:55:31 +0700

    test 2

--- src/Services/searchService.js
+++ src/Services/searchService.js
@@ -1,17 +1,18 @@
 import * as request from '~/utils/httpRequest';
 const get = async (q) => {
   try {
-    const res = await request.get('https://api-gateway.fullstack.edu.vn/api/search?', {
-      params: {
-        q,
+    const res = await request.get(
+      'https://api-gateway.fullstack.edu.vn/api/search?',
+      {
+        params: {
+          q,
+        },
       },
-    });
+      { headers: { 'Access-Control-Allow-Origin': '*' } },
+    );
     return res.data;
-    
   } catch (error) {
-    console.error(error.message)
-
-
+    console.error(error.message);
   }
 };
 
